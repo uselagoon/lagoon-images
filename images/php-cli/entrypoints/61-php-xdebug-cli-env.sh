c5009f3a97985457706295da3d9b72e1ffd72920
Dan Lemon <daniel.lemon@amazee.com>
2020-11-25 19:33:22 +0100

    Update variables from Xdebug 2 to Xdebug 3

--- images/php-cli/entrypoints/61-php-xdebug-cli-env.sh
+++ images/php-cli/entrypoints/61-php-xdebug-cli-env.sh
@@ -4,7 +4,7 @@
 if expr "$XDEBUG_ENABLE" : '[Tt][Rr][Uu][Ee]' > /dev/null; then
   # XDEBUG_CONFIG is used by xdebug to decide if an xdebug session should be started in the CLI or not.
   # The content doesn't really matter it just needs to be set, the actual connection details are loaded from /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-  export XDEBUG_CONFIG="idekey=lagoon"
+  export XDEBUG_SESSION="idekey=lagoon"
 
   # PHP_IDE_CONFIG is used by PhpStorm and should be the URL of the project, we use the `LAGOON_ROUTE` for it (if it exists)
   if [ ${LAGOON_ROUTE+x} ]; then

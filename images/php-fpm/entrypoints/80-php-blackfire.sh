3ea40a239ffc634c53f4388681c042e04497da4f
Michael Schmid <michael@amazee.com>
2021-06-30 16:33:42 -0400

    update to blackfire agent 2+

--- images/php-fpm/entrypoints/80-php-blackfire.sh
+++ images/php-fpm/entrypoints/80-php-blackfire.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # enable blackfire only if BLACKFIRE_ENABLED is set to true or TRUE or True
-if [[ "$BLACKFIRE_ENABLED" =~ [Tt][Rr][Uu][Ee] ]]; then
+if expr "$BLACKFIRE_ENABLED" : '[Tt][Rr][Uu][Ee]' > /dev/null; then
 
   # start the blackfire-agent
-  /bin/blackfire-agent --log-level="${BLACKFIRE_LOG_LEVEL:-3}" --socket="${BLACKFIRE_SOCKET:-"tcp://127.0.0.1:8307"}" &
+  /bin/blackfire agent --log-level="${BLACKFIRE_LOG_LEVEL:-3}" --socket="${BLACKFIRE_SOCKET:-"tcp://127.0.0.1:8307"}" &
 
   # envplate the blackfire ini file
   ep /usr/local/etc/php/conf.d/blackfire.disable

9e7f52e3362e4a26abcd491feee174c2ffdbc6d2
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-03-03 09:05:46 +0100

    Use proper environment variable to trigger XDebug in PHP CLI
    XDEBUG_SESSION can be used for backwards compatibility but the new
    key to use going forward for XDebug 3 is XDEBUG_TRIGGER.

--- images/php-cli/entrypoints/61-php-xdebug-cli-env.sh
+++ images/php-cli/entrypoints/61-php-xdebug-cli-env.sh
@@ -2,9 +2,9 @@
 
 # enable XDebug only if XDEBUG_ENABLE is set to true or TRUE or True
 if expr "$XDEBUG_ENABLE" : '[Tt][Rr][Uu][Ee]' > /dev/null; then
-  # XDEBUG_CONFIG is used by xdebug to decide if an xdebug session should be started in the CLI or not.
+  # XDEBUG_TRIGGER is used by xdebug to decide if an xdebug session should be started in the CLI or not.
   # The content doesn't really matter it just needs to be set, the actual connection details are loaded from /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-  export XDEBUG_SESSION="lagoon"
+  export XDEBUG_TRIGGER="cli"
 
   # PHP_IDE_CONFIG is used by PhpStorm and should be the URL of the project, we use the `LAGOON_ROUTE` for it (if it exists)
   if [ ${LAGOON_ROUTE+x} ]; then

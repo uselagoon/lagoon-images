f768551e1b9f43e056e17e0511b91a5e84fa8a24
Sean Hamlin <sean.hamlin@amazee.io>
2023-05-28 19:35:52 +1200

    Update the documentation inline the php configuration file.

--- images/php-fpm/entrypoints/70-php-config.sh
+++ images/php-fpm/entrypoints/70-php-config.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
 # In order for our custom PHP ini file to be loaded after the default PHP ini
-# configuration, we need to ensure it appears alphabetically after `php.ini`.
-# The filename `zz-php-lagoon.ini` was chosen to make this obvious.
+# configuration, we include this in the `conf.d` directory, this is read after
+# the default `php.ini` file.
+#
+# The file name is `00-lagoon-php.ini`, so if you want to override any further
+# values, you can craft any other `.ini` file in the `conf.d` directory to do
+# this. e.g.:
+#
+# $PHP_INI_DIR/
+#  ├─ php.ini
+#  ├─ conf.d/
+#  │  ├─ 00-lagoon-php.ini
+#  │  ├─ 01-your-further-overrides.ini
 #
 # The default `php.ini` is seeded from the default production values PHP
 # recommends.
